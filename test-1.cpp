bd304f21b409ce82d529ebb55bb6425fa9fa0167
ohuton55 <tttt.tttt.no@gmail.com>
2024-11-19 07:56:42 +0900

    print strings

--- test-1.cpp
+++ test-1.cpp
@@ -7,5 +7,11 @@ int main()
     cout << "Hello World\n";
     cout << 22.0 / 7.0; // double type
     cout << 'x';    // char type
-    cout << (5 & 6);    // int type 
+    cout << (5 & 6) << endl;    // int type 
+    
+    const char *s = "a fool, a pool: loopalootaloofa";
+    cout << s;  // print strings
+    cout << (void *)s << endl;  // print address to strings
+
+    cout << "the sum of" << 3 << " and " << 4 << " is " << 3+4 << '\n';
 }
