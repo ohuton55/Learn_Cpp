a074bd45085a1cc8af381b3c5e1d2aca8745be60
ohuton55 <tttt.tttt.no@gmail.com>
2024-11-19 07:42:17 +0900

    first commit

---
+++ test-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World" << endl;
+    cout << "Hello World\n";
+    cout << 22.0 / 7.0; // double type
+    cout << 'x';    // char type
+    cout << (5 & 6);    // int type 
+}
